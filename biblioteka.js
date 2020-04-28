bfe307ca2df21815d8dbd3545dbb4c69ccc2492e
Artur <arturmiks@gmail.com>
2020-04-28 19:48:47 +0200

    Dodano punkty przeciecia

--- biblioteka.js
+++ biblioteka.js
@@ -165,10 +165,44 @@ function policzOdcinkiOtoczki(punkty) {
   return odcinki;
 }
 
+function punktPrzecieciaOdcinkow(odcinekA, odcinekB)
+{
+  const p0_x = odcinekA.p1.x;
+  const p0_y = odcinekA.p1.y;
+  const p1_x = odcinekA.p2.x;
+  const p1_y = odcinekA.p2.y;
+
+  const p2_x = odcinekB.p1.x;
+  const p2_y = odcinekB.p1.y;
+  const p3_x = odcinekB.p2.x;
+  const p3_y = odcinekB.p2.y;
+
+  let s1_x, s1_y, s2_x, s2_y;
+  s1_x = p1_x - p0_x;     s1_y = p1_y - p0_y;
+  s2_x = p3_x - p2_x;     s2_y = p3_y - p2_y;
+
+  let s, t;
+  s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
+  t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+  if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+  {
+    return {
+      koliduje: true,
+      punkt: new Punkt(p0_x + (t * s1_x), p0_y + (t * s1_y), 'green'),
+    };
+  }
+
+  return {
+    koliduje: false,
+  };
+}
+
 window.lib = {
   Punkt,
   Odcinek,
   Polprosta,
   Prosta,
   policzOdcinkiOtoczki,
+  punktPrzecieciaOdcinkow,
 };
